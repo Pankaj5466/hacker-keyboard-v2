b293593dff93e3e19e05e0dd4a5cfef3fb570662
Pankaj Kumar <pankaj@Pankajs-MacBook-Air.local>
2024-12-20 20:39:21 +0530

    compact2 layout

--- app/src/main/java/org/pocketworkstation/pckeyboard/KeyboardSwitcher.java
+++ app/src/main/java/org/pocketworkstation/pckeyboard/KeyboardSwitcher.java
@@ -80,7 +80,10 @@ public class KeyboardSwitcher implements
     private static final int KBD_QWERTY = R.xml.kbd_qwerty;
     private static final int KBD_FULL = R.xml.kbd_full;
     private static final int KBD_FULL_FN = R.xml.kbd_full_fn;
-    private static final int KBD_COMPACT = R.xml.kbd_compact;
+//    private static final int KBD_COMPACT = R.xml.kbd_compact;
+//    private static final int KBD_COMPACT_TWO  = R.xml.kbd_compact_large_space; //TODO: (messiar87) enable and add logic so that both can be set indepedently
+
+    private static final int KBD_COMPACT = R.xml.kbd_compact_large_space;
     private static final int KBD_COMPACT_FN = R.xml.kbd_compact_fn;
 
     private LatinKeyboardView mInputView;
